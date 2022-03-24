8e0c64ffe315c905b0553985fe5bd5e1d707521e
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2022-03-24 12:37:11 +0100

    users seeder completed

--- seeder.js
+++ seeder.js
@@ -9,20 +9,20 @@ db.serialize(() => {
     db.run("INSERT INTO Users (firstname, lastname, email, password, img) VALUES ($firstname, $lastname, $email, $password, $img)", {
         $firstname: 'Micaela',
         $lastname: 'Milano',
-        $email: 'milano.micael@gmail.com',
-        $password: crypto.createHash('sha256').update('25Aprile87!').digest('hex'),
+        $email: 'micaela.example@gmail.com',
+        $password: crypto.createHash('sha256').update('Password1!').digest('hex'),
         $img: crypto.createHash('sha256').update('profilepicture').digest('hex') + '.jpg'
     });
     db.run("INSERT INTO Users (firstname, lastname, email, password) VALUES ($firstname, $lastname, $email, $password)", {
         $firstname: 'Marilisa',
         $lastname: 'Milano',
-        $email: 'milano.marilisa@gmail.com',
-        $password: crypto.createHash('sha256').update('09Novembre85!').digest('hex')
+        $email: 'marilisa.example@gmail.com',
+        $password: crypto.createHash('sha256').update('Password2!').digest('hex')
     });
     db.run("INSERT INTO Users (firstname, lastname, email, password) VALUES ($firstname, $lastname, $email, $password)", {
         $firstname: 'Mariachiara',
         $lastname: 'Milano',
-        $email: 'milano.mariachiara@gmail.com',
-        $password: crypto.createHash('sha256').update('07Agosto93!').digest('hex')
+        $email: 'mariachiara.example@gmail.com',
+        $password: crypto.createHash('sha256').update('Password3!').digest('hex')
     });
 });
