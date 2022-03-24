bb279f16e2431ca172c070525461f40ea94e72ad
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2022-03-24 12:21:51 +0100

    server init

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const _ = require('lodash');
+const path = require('path');
+const user = require('./api/user.js');
+
+const app = express();
+
+app.use('/api/user', user);
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 4001;
+
+app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
