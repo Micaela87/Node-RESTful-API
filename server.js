0695f0b0d095aed0e66de6dcaf7bfd92086e586c
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2022-04-11 17:57:42 +0200

    csurf configured

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const _ = require('lodash');
 const path = require('path');
 const sessions = require('express-session');
 const cookieParser = require('cookie-parser');
+const csurf = require('csurf');
+const hbs = require('hbs');
 const SQLiteStore = require('connect-sqlite3')(sessions);
 
 // express routers
 const user = require('./api/routes/user.js');
-
 const app = express();
+const oneDay = 1000 * 60 * 60 * 24; // cookie expires after 24h in milliseconds
+const csrfMiddleware = csurf({
+    cookie: {
+        maxAge: oneDay
+    }
+
+});
+
+// sets handlebars as template engine
+app.set('view engine', 'hbs');
 
 // allows communication between client and server
 app.use(cors());
@@ -28,7 +39,6 @@ app.use(express.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 
 // sessions config
-const oneDay = 1000 * 60 * 60 * 24; // cookie expires after 24h in milliseconds
 app.use(sessions({
     store: new SQLiteStore,
     secret: "keyboard cat",
@@ -40,7 +50,14 @@ app.use(sessions({
 // cookie parser
 app.use(cookieParser());
 
+app.use(csrfMiddleware);
+
 app.use('/api/user', user);
+
+// renders index page with handlebars template engine
+app.get('/', (req, res)=>{
+    res.render('index', { csrfToken: req.csrfToken() });
+});
 
 // starts the server on port 4001 if no process.env.PORT is set
 const port = process.env.PORT || 4001;
@@ -49,4 +66,5 @@ app.listen(port, function() {
     console.log('Server listening on port ' + port);
 });
 
+// exports app to be tested
 module.exports = app;
